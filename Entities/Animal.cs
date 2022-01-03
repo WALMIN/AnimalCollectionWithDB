05081023cf4bf1de8a1d83be405646d1c2a93ac6
Victor Bergene <v@walmin.com>
2022-01-03 16:41:44 +0100

    Added entities

---
+++ Entities/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimalCollectionWithDB.Entities
+{
+    public class Animal
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int AnimalTypeID { get; set; }
+        public AnimalType AnimalType { get; set; }
+    }
+}
---
+++ Entities/AnimalType.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimalCollectionWithDB.Entities
+{
+    public class AnimalType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Animal> Animals { get; set; }
+    }
+}
